8135ec7c62fd1d81865de5044fc8f2f49dd3995d
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-10-09 09:32:27 -0700

    Bring ./target back

--- js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [robot-overlord.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/robot-overlord.core.on-js-reload (apply js/robot-overlord.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'robot-overlord.core/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
