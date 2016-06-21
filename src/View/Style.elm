669c463027a6e218930443415fbae9184d6e84d6
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-06-21 17:25:59 +0900

    Get elm-css working (it's pretty great)

---
+++ src/View/Style.elm
@@ -0,0 +1,22 @@
+port module Stylesheets exposing (..)
+
+import Css.File exposing (..)
+import View.Style.Base exposing (css)
+import Html exposing (div)
+import Html.App as Html
+
+
+port files : CssFileStructure -> Cmd msg
+
+cssFiles : CssFileStructure
+cssFiles =
+    toFileStructure [ ( "styles.css", compile css ) ]
+
+main : Program Never
+main =
+    Html.program
+        { init = ( (), files cssFiles )
+        , view = \_ -> (div [] [])
+        , update = \_ _ -> ( (), Cmd.none )
+        , subscriptions = \_ -> Sub.none
+        }
---
+++ src/View/Style/Base.elm
@@ -0,0 +1,47 @@
+module View.Style.Base exposing (..)
+
+import Css exposing (..)
+import Css.Elements exposing (body, li)
+import Css.Namespace exposing (namespace)
+
+
+type CssClasses
+    = NavBar
+
+
+type CssIds
+    = Page
+
+
+css : Stylesheet
+css =
+    (stylesheet << namespace "dreamwriter")
+    [ body
+        [ overflowX auto
+        , minWidth (px 1280)
+        ]
+    , (#) Page
+        [ backgroundColor (rgb 200 128 64)
+        , color (hex "CCFFFF")
+        , width (pct 100)
+        , height (pct 100)
+        , boxSizing borderBox
+        , padding (px 8)
+        , margin zero
+        ]
+    , (.) NavBar
+        [ margin zero
+        , padding zero
+        , children
+            [ li
+                [ (display inlineBlock) |> important
+                , color primaryAccentColor
+                ]
+            ]
+        ]
+    ]
+
+
+primaryAccentColor : Color
+primaryAccentColor =
+    hex "ccffaa"
