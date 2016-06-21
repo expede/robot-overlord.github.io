cae9c49d9a84dde3410aa7b9fdb8ee6990b9ecb3
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-06-21 17:43:08 +0900

    Define colour palatte

---
+++ src/View/Style/Color.elm
@@ -0,0 +1,31 @@
+module View.Style.Color exposing (base, neutral, highlight, accent)
+
+import Css exposing (Color, rgb)
+
+neutral : Color
+neutral = white
+
+base : Color
+base = navy
+
+highlight : Color
+highlight = lightBlue
+
+accent : Color
+accent = pink
+
+--------------------------------
+-- Primitives: do not export! --
+--------------------------------
+
+pink : Color
+pink = rgb 236 112 119
+
+lightBlue : Color
+lightBlue = rgb 102 216 213
+
+navy : Color
+navy = rgb 34 40 50
+
+white : Color
+white = rgb 255 255 255
