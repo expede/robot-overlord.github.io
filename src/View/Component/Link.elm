876585b4fb3aa3ac938c649ea5e7593d95d17d64
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-07-04 21:44:03 +0900

    Type signature typo

--- src/View/Component/Link.elm
+++ src/View/Component/Link.elm
@@ -5,13 +5,15 @@ import Html exposing (Html, a, text)
 import Html.Attributes exposing (href)
 import Html.Events exposing (onClick)
 
+import VirtualDom exposing (Property)
+
 import Router.Route exposing (Route(..))
 import Signal exposing (Signal(..))
 
-link : List Signal -> Route -> String -> Html (List Signal)
+link : List (Property Signal) -> Route -> String -> Html (List Signal)
 link attrs route string =
   a [ href "javascript://"
-    , onClick <| NavigateTo route
+    , onClick (NavigateTo route)
     :: attrs
     ]
     [ text string ]
