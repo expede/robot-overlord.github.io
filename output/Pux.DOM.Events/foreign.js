340c2751008f58f28d096b1b67706084374c7a0e
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-06-26 00:09:19 -0700

    Don't commit output

--- output/Pux.DOM.Events/foreign.js
+++
@@ -1,7 +0,0 @@
-exports.targetValue = function (ev) {
-  var target = ev['target'];
-  if (target === undefined) return '';
-  var value = target['value'];
-  if (value === undefined) return '';
-  return value;
-};
