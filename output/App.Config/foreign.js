b942832c63a96e5c455c82bd5e265a15d72dc877
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-06-25 23:36:11 -0700

    Compatible dev and prod environments

--- output/App.Config/foreign.js
+++ output/App.Config/foreign.js
@@ -2,5 +2,5 @@ exports.config = {
   title: 'Robot Overlord Software',
   public_path: process.env.NODE_ENV === 'production'
                ? '/dist/'
-               : 'http://localhost:8080/dist/'
+               : 'http://localhost:8080/static/dist/'
 }
--- support/entry.js
+++ support/entry.js
@@ -1,3 +1,4 @@
+require('normalize-css');
 require('../src/style.scss');
 let ClientEntry = require('../src/Main.purs');
 let app = ClientEntry.main(window.location.hash)(window.__puxLastState || ClientEntry.initialState)(window.location.hash)
