fb433f83ae0d7da7654a7cd0f968c5fee3063834
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-07-16 17:47:03 -0700

    Email subscription

--- src/App/View/Homepage.purs
+++ src/App/View/Homepage.purs
@@ -1,15 +1,13 @@
 module App.View.Homepage where
 
+import Text.Smolder.HTML
+
 import App.Events (Event)
 import App.State (State)
-
 import Control.Bind (discard)
 import Data.Function (($))
-
 import Pux.DOM.HTML (HTML)
-
-import Text.Smolder.HTML
-import Text.Smolder.HTML.Attributes (id, src, type', placeholder, method, action, name)
+import Text.Smolder.HTML.Attributes (action, id, method, name, placeholder, src, target, type', value)
 import Text.Smolder.Markup ((!), text)
 
 view :: State -> HTML Event
@@ -27,11 +25,15 @@ view _ =
 
         h2 $ text "Changing the way the world writes software"
 
+      span ! id "continue" $ do
+        text "▼"
+
     section ! id "contact-form" $ do
       h1 $ text "Get in touch"
 
       form ! method "POST"
-           ! action "http://formspree.io/beep@robotoverlord.io" $ do
+           ! action "http://formspree.io/beep@robotoverlord.io"
+           $ do
 
         input ! type' "email"
               ! name "email"
@@ -44,8 +46,35 @@ view _ =
         button ! type' "submit"
                $ text "Send"
 
-      -- span ! id "continue" $ do
-      --   text "▼"
+      h1 $ text "Stay up to date"
+      h2 $ text "Updates, offers, tips, and more"
+
+      form ! method "POST"
+           ! action "https://goodbits.io/e/556cc53a-be05-41d2-9be0-eafda4340f47"
+           ! target "_blank"
+           $ do
+
+        input ! type' "text"
+              ! name  "first_name"
+              ! placeholder "First Name"
+
+        input ! type' "text"
+              ! name  "last_name"
+              ! placeholder "Last Name"
+
+        input ! type' "text"
+              ! name  "email"
+              ! placeholder "Email"
+
+        input ! type' "submit"
+              ! value "Subscribe"
+
+-- <form method="post" action="https://goodbits.io/e/556cc53a-be05-41d2-9be0-eafda4340f47" target="_blank">
+--   <input type="text" name="first_name" placeholder="First Name"></input>
+--   <input type="text" name="last_name" placeholder="Last Name"></input>
+--   <input type="text" name="email" placeholder="Email"></input>
+--   <input type="submit" value="Subscribe">
+-- </form>
 
     -- section ! id "pagey" $ do
     --   section do
