3440d38e06e5f05525009bf0ee51742f6328cd30
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-09-23 00:12:37 -0700

    Placeholder images

--- src/App/Config.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-  title: 'Robot Overlord: Elixir and Elm Training',
-  description: 'Functional programming corporate training. Languges, libraries, and techniques. Elixir, Elm, Clojure, React, Lodash, and beyond. Based out of Vancouver, Canada, we offer classes with expert instructors in both retreats and on-site.',
-  public_path: process.env.NODE_ENV === 'production'
-               ? '/dist/'
-               : 'http://localhost:8080/static/dist/'
-}
