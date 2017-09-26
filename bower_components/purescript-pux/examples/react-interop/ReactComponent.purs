28eb911b9d6a8a0cd75d88ad9a5cfdca1660fba3
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-09-25 23:37:14 -0700

    Labbé redesign (#12)

--- bower_components/purescript-pux/examples/react-interop/ReactComponent.purs
+++
@@ -1,8 +0,0 @@
-module ReactInteropExample.ReactComponent where
-
-import React (ReactClass)
-import Pux.Renderer.React (reactClassWithProps)
-
-foreign import component_ :: ∀ props. ReactClass props
-
-component = reactClassWithProps component_ "MyComponent"
--- src/App/Config.purs
+++
@@ -1,9 +0,0 @@
-module App.Config where
-
-type Config =
-  { title       :: String
-  , description :: String
-  , public_path :: String
-  }
-
-foreign import config :: Config
